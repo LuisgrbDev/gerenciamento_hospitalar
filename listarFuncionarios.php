3c8a4d82cbfc2bd2405ef9285602fbaed3d65b9f
luisgrbDev_senac <luisgrbenvindo@gmail.com>
2024-06-26 20:24:36 -0300

    Controller e Model Funcionario Criada e Funcional

---
+++ listarFuncionarios.php
@@ -0,0 +1,19 @@
+<?php
+require_once("backend/services/db.php");
+require_once("backend//controller/controllerFuncionarios.php");
+require_once("backend/model/modelFuncionarios.php");
+
+
+$controllerFuncionarios = new controllerFuncionarios();
+$resultado = $controllerFuncionarios->listarFuncionarios();
+
+
+if($resultado) {
+    $result = array("Funcionario", $resultado);
+    echo json_encode($result);
+} else {
+    echo ("deu erro");
+}
+
+
+?>
